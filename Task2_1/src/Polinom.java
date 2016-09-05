0445e9e25f927ac952d987dd631a1474e23a044e
Viktorija Melkozerova <vika102588@mail.ru>
2016-09-06 02:27:51 +0400

    Diff Task_2

--- Task2_1/src/Polinom.java
+++ Task2_1/src/Polinom.java
@@ -1,9 +1,7 @@
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
-/**
- * Created by Pusja on 06.09.2016.
- */
+
 public class Polinom {
 
     public static void printPol(int n) {
@@ -17,18 +15,19 @@ public class Polinom {
         int n = 0;
         try {
             System.out.println("Введите число для разложения на слагаемые:");
-            System.out.println("Введите N:");
+            System.out.println("Введите N(N<=100):");
             n = in.nextInt();
-            int j;
-            for (j = 0; j < n; j++) {
-                System.out.printf("%d = %d ", n, n-j);
-                printPol(j);
-                System.out.println("\n");
-            }
-
+            if (N <= 100) {
+                int j;
+                for (j = 0; j < n; j++) {
+                    System.out.printf("%d = %d ", n, n - j);
+                    printPol(j);
+                    System.out.println("\n");
+                }
+            } else
+                System.out.println("N<=100!!!");
         } catch (InputMismatchException e) {
             System.out.println("Вы ввели не Int число");
-            //    throw e;
         }
     }
 }
